638004e9419c62e9abc075270a4f0ac2d66bddd3
Sahil Shah <sahilshah2104@gmail.com>
2024-04-30 14:51:29 +0530

    Feat: Added Eslint Configs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, { Express } from "express";
+import express from "express";
+
 import serverConfig from "./config/server.config";
 
-const app: Express = express();
+const app = express();
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server started at PORT ${serverConfig.PORT}`);
