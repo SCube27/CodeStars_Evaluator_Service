70afe7143ad762b96dea3feaaaca4aa626ba7864
Sahil Shah <sahilshah2104@gmail.com>
2024-05-01 13:06:09 +0530

    Feat: BullMQ based Redis Queueing Infra up and running

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 
 import serverConfig from "./config/server.config";
+import sampleQueueProducer from "./producers/sampleQueue.producer";
 import apiRouter from "./routes";
+import SampleWorker from "./workers/sample.worker";
 
 const app = express();
 
@@ -9,4 +11,13 @@ app.use('/api', apiRouter);
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server started at PORT ${serverConfig.PORT}`);
+
+    SampleWorker('SampleQueue');
+
+    sampleQueueProducer('SampleJob', {
+        name: 'Sahil Shah',
+        company: 'Barclays',
+        position: 'Software Engineer',
+        location: 'Remote | PUN | MUM'
+    });
 });
--- src/jobs/sample.job.ts
+++ src/jobs/sample.job.ts
@@ -10,8 +10,12 @@ export default class SampleJob implements IJob {
         this.name = this.constructor.name;
     }
 
-    handle = () => {
+    handle = (job?: Job) => {
         console.log("Handler of the Job called");
+        if(job) {
+            console.log(this.payload);
+            console.log(job.name, job.id, job.data);
+        }
     };
 
     failed = (job?: Job): void => {
